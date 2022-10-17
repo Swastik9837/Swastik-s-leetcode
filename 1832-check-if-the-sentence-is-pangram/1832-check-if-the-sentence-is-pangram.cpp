9e5c18781ef9b330eb94842fbc8069921582e2e2
Swastik Rana <107426987+Swastik9837@users.noreply.github.com>
2022-10-17 20:14:26 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (6.60%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,22 +1,10 @@
 class Solution {
 public:
-   
     bool checkIfPangram(string sentence) {
-        // we iterate over 'sentence' for 26 times, one for each letter 'currChar'
-       
-        for(int i=0;i<26;i++)
-        {
-            char currChar='a' + i;
-            
-            //if sentence doen't contain currChar, it is not a pangram
-            if(sentence.find(currChar) ==  string::npos)
-            {
-                return false;
-            }
-            
-        }
-        return true;
-        
+         // Add every letter of 'sentence' to hash set 'seen'.
+        unordered_set<char> seen(sentence.begin(), sentence.end());
+         // If the size of 'seen' is 26, then 'sentence' is a pangram.
+        return seen.size() == 26;
         
     }
 };
