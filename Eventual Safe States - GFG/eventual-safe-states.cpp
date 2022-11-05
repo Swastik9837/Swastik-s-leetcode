f7e3cad8960a7dd37b4a58e392bb1209a4adc36f
Swastik Rana <107426987+Swastik9837@users.noreply.github.com>
2022-11-05 18:44:59 +0530

    Added solution - LeetHub

---
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+     private:
+    bool dfsCheck(int node,vector<int>adj[] , int vis[] , int pathVis[], int check[]){
+        vis[node]=1;
+        pathVis[node]=1;
+        check[node]=0;
+         //traverse for adjacent nodes
+         for(auto it : adj[node]){
+             //this is when the node is not visited
+             if(!vis[it]){
+                 // if this made a cycle then return true and no safe node
+                 if(dfsCheck(it,adj,vis,pathVis,check)==true){
+                     check[node]=0;
+                     return true;
+                     
+                 }
+             }
+             // if the node has been previously visited
+             //but it has to be vistied on the same path
+             else if(pathVis[it]){
+                 
+                 check[node]=0;
+                 return true;
+             }
+         }
+         check[node]=1; // if it reaches this line it means no cycle is found
+        pathVis[node]=0;  // again set pathVis to 0;
+        return false; 
+    }
+  public:
+    vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
+        // code here
+         // code here
+        int vis[V]={0};
+        int pathVis[V]={0};// both are 0based indexing
+        int check[V]={0};
+        vector<int> safeNodes;
+        
+        for(int i=0;i<V;i++)
+        {
+            if(!vis[i]){
+               dfsCheck(i,adj,vis,pathVis,check);
+            }
+        }
+        
+        for(int i=0;i<V;i++)
+        {
+            if(check[i]==1)safeNodes.push_back(i);
+        }
+        return safeNodes;
+ 
+    }
+    
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+
+        Solution obj;
+        vector<int> safeNodes = obj.eventualSafeNodes(V, adj);
+        for (auto i : safeNodes) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+
+// } Driver Code Ends
